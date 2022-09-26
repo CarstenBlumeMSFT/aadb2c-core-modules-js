ba09d9f518e61dcbe294946191a3c87f78665b1e
Carsten Blume <carsten.blume@microsoft.com>
2022-09-26 11:50:30 +0200

    changed export style.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-import ILogger, {ConsoleLogger} from './logger-interface'
-import {MSALClientCredentialsAuthProvider} from './msal-auth'
-import PolicyBuild from './policy-build'
-import PolicyUpload from './policy-upload'
-import RenumberPolicies from './renumber-steps'
-
-export {ILogger, ConsoleLogger}
-export {MSALClientCredentialsAuthProvider}
-export {PolicyBuild}
-export {PolicyUpload}
-export {RenumberPolicies}
+export * from './logger-interface'
+export * from './msal-auth'
+export * from './policy-build'
+export * from './policy-upload'
+export * from './renumber-steps'
